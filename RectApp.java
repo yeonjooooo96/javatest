306acbd6f504d9c30635794d5be3d72255fb1265
yeonjooooo96 <kyeonjooooo@naver.com>
2020-03-26 16:39:35 +0900

    step4

---
+++ RectApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Rectangle{
+	int width;
+	int height;
+	public int getArea(){
+		return width*height;
+	}
+}
+
+public class RectApp{
+	public static void main(String[] args){
+		Rectangle rect=new Rectangle();
+		Scanner scanner=new Scanner(System.in);
+		System.out.print(">>");
+		rect.width=scanner.nextInt();
+		rect.height=scanner.nextInt();
+		System.out.println("result:"+rect.getArea());
+		scanner.close();
+	}
+}
+
